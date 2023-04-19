1d3543b6521b48f742d423a0afac3c329864d615
JespervanBerlo <vanberlojesper@gmail.com>
2023-04-19 02:40:37 +0200

    add drawing app

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -5,7 +5,32 @@ const Canvas = ({
     height
 }) => {
 
-    const setCanvasRef = useOnDraw();
+    const setCanvasRef = useOnDraw(onDraw);
+
+    function onDraw(ctx, point, prevPoint) {
+        drawLine(prevPoint, point, ctx, '#0000FF', 3);
+    }
+
+    function drawLine(
+        start,
+        end,
+        ctx,
+        color,
+        width
+    ) {
+        start = start ?? end;
+        ctx.beginPath();
+        ctx.lineWidth = width;
+        ctx.strokeStyle = color;
+        ctx.moveTo(start.x, start.y);
+        ctx.lineTo(end.x, end.y);
+        ctx.stroke()
+
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(start.x, start.y, 2, 0, 2 * Math.PI);
+        ctx.fill();
+    }
 
     return(
         <canvas 
