85b38d6418cf5f6e51bc576a38ef6112984adb74
JespervanBerlo <vanberlojesper@gmail.com>
2023-08-24 04:15:06 +0200

    finish clock

--- src/Components/Klok.js
+++ src/Components/Klok.js
@@ -38,6 +38,20 @@ function Klok(){
             style={{transform: `rotateZ(${date.getSeconds() * 6 - 180}deg)`}}
             >
             </div>
+            <div className="hourmarks">
+                <div index="0">12</div>
+                <div index="1">1</div>
+                <div index="2">2</div>
+                <div index="3">3</div>
+                <div index="4">4</div>
+                <div index="5">5</div>
+                <div index="6">6</div>
+                <div index="7">7</div>
+                <div index="8">8</div>
+                <div index="9">9</div>
+                <div index="10">10</div>
+                <div index="11">11</div>
+            </div>
         </div>
     )
 }
